8170ed921f63dd8f6913bef0eb8f8a0c24c3e667
김성주 <aaa8757k@gmail.com>
2025-08-21 15:58:11 +0900

    feat:린트 임포트 오더 추가, speed insight 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,14 +12,50 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    ignores: ['node_modules/**', '.next/**', 'out/**'],
     rules: {
       'no-unused-vars': 'off', // JS용 기본 비활성화
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
+
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@src/**', // alias 사용 시 맞춰 변경
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
+
   },
+
 ];
 
 export default eslintConfig;
