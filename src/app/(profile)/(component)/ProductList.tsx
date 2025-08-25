2a2d79733f4fd298f5c2b8738158bf8c419af87c
김성주 <aaa8757k@gmail.com>
2025-08-25 12:44:34 +0900

    fix: 활동카드, 상품카드 배경색 수정

--- src/app/(profile)/(component)/ProductList.tsx
+++ src/app/(profile)/(component)/ProductList.tsx
@@ -24,7 +24,10 @@ const ProductList = ({ userid }: Props) => {
       <DropdownTrigger />
       <ul className='flex max-w-[940px] flex-wrap gap-[15px] xl:gap-5'>
         {movieList.map((movie) => (
-          <li className='rounded-[8px] bg-[var(--color-black-353542)] p-2.5 xl:p-5' key={movie.id}>
+          <li
+            className='rounded-[8px] border-[var(--color-black-353542)] bg-[var(--color-black-252530)] p-2.5 xl:p-5'
+            key={movie.id}
+          >
             {/* 컨텐츠 영화로 했을 때는 세로로 좀 더 길어야 할 것 같은데 고민이네요 */}
             <div className='mb-2.5 h-[98px] w-[140px] bg-amber-800 md:mb-5 md:h-[160px] md:w-[227px] xl:mb-[25px] xl:h-[200px] xl:w-[260px]' />
             <div className='flex flex-col'>
