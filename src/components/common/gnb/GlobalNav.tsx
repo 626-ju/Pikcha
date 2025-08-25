14654bbb13581fe4f855d833465368e86c036d23
Sophia Na (나소연) <devsophia029@gmail.com>
2025-08-25 09:35:22 +0900

    feat: 글로벌 네비게이션 추가

--- src/components/common/gnb/GlobalNav.tsx
+++ src/components/common/gnb/GlobalNav.tsx
@@ -1,46 +1,33 @@
-import MobileMenu from './mobileMenu/MobileMenu';
-import MobileSearch from './mobileSearch/MobileSearch';
-import ProfileDropdown from './profile/ProfileDropdown';
-import SearchForm from './searchForm/SearchForm';
+import DesktopCenter from './section/DesktopCenter';
+import DesktopLeft from './section/DesktopLeft';
+import DesktopRight from './section/DesktopRight';
+import MobileCenter from './section/MobileCenter';
+import MobileLeft from './section/MobileLeft';
+import MobileRight from './section/MobileRight';
 
-import type { HeaderConfig, HeaderItem } from '@/types/Gnb.type';
-
-const ItemMap: Record<HeaderItem, React.ComponentType> = {
-  logo: Logo,
-  search: SearchForm,
-  login: Login,
-  signup: Signup,
-  compare: Compare, // 클라이언트로 가야함.
-  profile: ProfileDropdown, // 클라이언트
-  'mobile-menu': MobileMenu, // 클라이언트
-  'mobile-search': MobileSearch, // 클라이언트
-};
-
-function Render(items: readonly HeaderItem[]) {
-  return items.map((k) => {
-    const C = ItemMap[k];
-    return <C key={k} />;
-  });
-}
-
-const GlobalNav = ({ config }: { config: HeaderConfig }) => {
+const GlobalNav = () => {
   return (
-    <header className='bg-background/80 sticky top-0 z-50 border-b backdrop-blur-md'>
-      <div className='mx-auto flex h-[100px] items-center gap-2 px-3'>
-        {/* 모바일*/}
-        <div className='flex items-center gap-2 md:hidden'>{Render(config.mobile.left)}</div>
+    <header className='border-black-2e2e3a md:px=[30px] relative z-50 h-[70px] border-b-2 shadow-sm shadow-black md:h-[80px] xl:h-[100px] xl:px-[120px]'>
+      <div className='mx-auto flex h-full items-center gap-4 px-5'>
+        {/*모바일*/}
+        <div className='flex items-center gap-2 md:hidden'>
+          <MobileLeft />
+        </div>
         <div className='grid flex-1 place-items-center md:hidden'>
-          {Render(config.mobile.center)}
+          <MobileCenter />
         </div>
-        <div className='flex items-center gap-1 md:hidden'>{Render(config.mobile.right)}</div>
-
-        {/* 데스크톱 */}
-        <div className='hidden w-1/3 items-center gap-2 md:flex'>{Render(config.desktop.left)}</div>
-        <div className='hidden w-1/3 items-center justify-center md:flex'>
-          {Render(config.desktop.center)}
+        <div className='flex items-center gap-1 md:hidden'>
+          <MobileRight />
         </div>
-        <div className='hidden w-1/3 items-center justify-end gap-2 md:flex'>
-          {Render(config.desktop.right)}
+        {/*데스크톱*/}
+        <div className='hidden w-2/7 items-center gap-2 md:flex xl:w-2/12'>
+          <DesktopLeft />
+        </div>
+        <div className='hidden w-3/7 items-center justify-end md:flex xl:w-8/12'>
+          <DesktopCenter />
+        </div>
+        <div className='hidden w-2/7 items-center justify-end gap-2 px-3 md:flex xl:w-2/12'>
+          <DesktopRight isLoggedIn={true} /> {/* auth 연결 이후 프롭으로 내려줄 예정 */}
         </div>
       </div>
     </header>
