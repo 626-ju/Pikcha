508e62cf526037d548d64c03b046ed70812b782c
김성주 <aaa8757k@gmail.com>
2025-08-25 13:37:03 +0900

    chore: 드랍다운 트리거 client 명시

--- src/app/(profile)/(component)/DropdownTrigger.tsx
+++ src/app/(profile)/(component)/DropdownTrigger.tsx
@@ -1,13 +1,9 @@
+'use client';
+
 import React from 'react';
 
-import SortDropdown from '@/components/common/dropdowns/SortDropdown';
-
 const DropdownTrigger = () => {
-  return (
-    <div className='text-mogazoa-18px-600 mt-15 mb-7.5 xl:mt-20'>
-      <SortDropdown variant={'product'} />
-    </div>
-  );
+  return <div className='text-mogazoa-18px-600 mt-15 mb-7.5 xl:mt-20'>드랍다운 자리</div>;
 };
 
 export default DropdownTrigger;
