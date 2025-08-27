8ad52e349f6ff0683c3d1a14db6e94831c08aa30
10000jaeN <akswo256@naver.com>
2025-08-27 17:19:30 +0900

    LIN-19 chore: className 적용방식 수정

--- src/components/ui/chips/RankingChip.tsx
+++ src/components/ui/chips/RankingChip.tsx
@@ -1,23 +1,22 @@
+import { cn } from '@/lib/utils';
 import { RankingChipProps } from '@/types/chips';
 
 const RankingChip = ({ idx, className }: RankingChipProps) => {
-  const baseStyle =
-    'flex shrink-0 px-[8px] py-[2px] rounded-full text-mogazoa-10px-400 xl:text-mogazoa-12px-400';
-
-  let rankingStyle;
   const ranking = `${idx + 1}등`;
 
-  if (idx === 0) {
-    rankingStyle = 'bg-[#FF2F9F]/10 text-[#FF2F9F]';
-  } else if (idx === 1) {
-    rankingStyle = 'bg-[#05D58B]/10 text-[#05D58B]';
-  } else if (idx >= 2) {
-    rankingStyle = 'bg-gray-9fa6b2/10 text-gray-9fa6b2';
-  }
-
-  const allClassName = `${baseStyle} ${rankingStyle} ${className}`;
-
-  return <span className={allClassName}>{ranking}</span>;
+  return (
+    <span
+      className={cn(
+        'text-mogazoa-10px-400 xl:text-mogazoa-12px-400 flex shrink-0 rounded-full px-[8px] py-[2px]',
+        idx === 0 && 'bg-[#FF2F9F]/10 text-[#FF2F9F]',
+        idx === 1 && 'bg-[#05D58B]/10 text-[#05D58B]',
+        idx >= 2 && 'bg-gray-9fa6b2/10 text-gray-9fa6b2',
+        className,
+      )}
+    >
+      {ranking}
+    </span>
+  );
 };
 
 export default RankingChip;
