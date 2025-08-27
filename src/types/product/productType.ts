2c13fbce839502c0aa52b5e3bf0f2ef7447f9c46
10000jaeN <akswo256@naver.com>
2025-08-27 14:43:48 +0900

    LIN-15 chore: 타입 추가

--- src/types/product/productType.ts
+++ src/types/product/productType.ts
@@ -23,3 +23,33 @@ export interface Product {
     reviewCount: number;
   };
 }
+
+export interface ProductDetail {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  rating: number;
+  reviewCount: number;
+  favoriteCount: number;
+  categoryId: number;
+  createdAt: string; //ISO 8601 형식
+  updatedAt: string; //ISO 8601 형식
+  writerId: number;
+  isFavorite: boolean;
+  category: {
+    id: number;
+    name: string;
+  };
+  categoryMetric: {
+    rating: number;
+    favoriteCount: number;
+    reviewCount: number;
+  };
+}
+
+export interface CalculationMapProps {
+  rating: (product: ProductDetail) => number;
+  favorite: (product: ProductDetail) => number;
+  review: (product: ProductDetail) => number;
+}
