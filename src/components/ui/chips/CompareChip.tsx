6b218698bef22d9f31f3040a8a9e64dd570beb87
10000jaeN <akswo256@naver.com>
2025-08-27 17:45:10 +0900

    LIN-19 chore: compareChip hover스타일 변경

--- src/components/ui/chips/CompareChip.tsx
+++ src/components/ui/chips/CompareChip.tsx
@@ -8,14 +8,18 @@ const CompareChip = ({ variant, productName, onClick }: CompareChipProps) => {
   return (
     <div
       className={cn(
-        'text-mogazoa-16px-400 flex shrink-0 items-center gap-[10px] rounded-[6px] px-[10px] py-2',
+        'text-mogazoa-16px-400 group flex shrink-0 cursor-pointer items-center gap-[10px] rounded-[6px] px-[10px] py-2',
         variant === 'first' ? 'bg-[#05D58B]/10 text-[#05D58B]' : 'bg-[#FF2F9F]/10 text-[#FF2F9F]',
       )}
+      onClick={onClick}
     >
       {productName}
       <button
-        onClick={onClick}
-        className='flex h-[19px] w-[19px] cursor-pointer items-center justify-center rounded-[6px] bg-[#000000]/50'
+        className={cn(
+          'flex h-[19px] w-[19px] cursor-pointer items-center justify-center rounded-[6px]',
+          variant === 'first' ? 'group-hover:bg-[#05D58B]/60' : 'group-hover:bg-[#FF2F9F]/60',
+          'transition-colors duration-200 group-hover:scale-110',
+        )}
       >
         <DeleteIcon />
       </button>
