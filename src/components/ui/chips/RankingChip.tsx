bd62c67e7ecb205c8a67f12ed6537bcc2cf32554
Sophia Na (나소연) <devsophia029@gmail.com>
2025-08-28 00:34:37 +0900

    feat: 랭킹칩 내 가운데 정렬 추가

--- src/components/ui/chips/RankingChip.tsx
+++ src/components/ui/chips/RankingChip.tsx
@@ -7,7 +7,7 @@ const RankingChip = ({ idx, className }: RankingChipProps) => {
   return (
     <span
       className={cn(
-        'text-mogazoa-10px-400 xl:text-mogazoa-12px-400 flex shrink-0 rounded-full px-[8px] py-[2px]',
+        'text-mogazoa-10px-400 xl:text-mogazoa-12px-400 flex shrink-0 items-center rounded-full px-[8px] py-[2px]',
         idx === 0 && 'bg-[#FF2F9F]/10 text-[#FF2F9F]',
         idx === 1 && 'bg-[#05D58B]/10 text-[#05D58B]',
         idx >= 2 && 'bg-gray-9fa6b2/10 text-gray-9fa6b2',
