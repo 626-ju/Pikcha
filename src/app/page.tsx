ccb28a87205b3b3f46e6b5c67b18354ce93db05b
Sophia Na (나소연) <devsophia029@gmail.com>
2025-08-28 21:54:25 +0900

    feat: 랜딩 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,55 @@
 import { getReviewerRanking } from '@/actions/review/reviewer';
 
-import ReviewerList from './_components/reviewer/ReviewerList';
+import { searchProducts } from '../actions/productList';
+import NoResult from './_components/NoResult';
+import RatingRank from './_components/RatingRank';
+import ResultTitle from './_components/ResultTitle';
+import ReviewerRank from './_components/reviewer/ReviewerRank';
+import ReviewRank from './_components/ReviewRank';
+import SearchResultList from './_components/SearchResultList';
+import Sidebar from './_components/Sidebar';
 
-const Home = async () => {
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: { q?: string; categoryId?: string; cursor?: string };
+}) => {
   const users = await getReviewerRanking();
-  if (!users?.length) {
-    return <p>랭킹 데이터가 없습니다.</p>; // 추후 수정 필요
-  }
-  return <ReviewerList users={users} />;
+  const q = searchParams.q ?? '';
+  const category = searchParams.categoryId ? Number(searchParams.categoryId) : null;
+  const cursor = searchParams.cursor ? Number(searchParams.cursor) : null;
+
+  const data = await searchProducts({ q, category, cursor });
+  const items = data.list;
+
+  return (
+    <div>
+      <div className='flex gap-6'>
+        <Sidebar selected={category} q={q} />
+        <section className='flex-1 px-[20px] pt-[60px]'>
+          <ReviewerRank users={users} className='mb-6 xl:hidden' />
+          {q === '' && category === null ? (
+            <div className='flex flex-col gap-[80px]'>
+              <ReviewRank />
+              <RatingRank />
+            </div>
+          ) : items.length === 0 ? (
+            <NoResult />
+          ) : (
+            <div>
+              <ResultTitle category={category} q={q} />
+              <SearchResultList
+                initialProducts={items}
+                initialCursor={data.nextCursor}
+                searchParams={{ q, category }}
+              />
+            </div>
+          )}
+        </section>
+        <ReviewerRank users={users} className='hidden w-0 xl:flex xl:w-[300px] xl:justify-start' />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
