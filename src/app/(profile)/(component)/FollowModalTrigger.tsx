3f83b957128352380d1f2650c80d1690c8e1cde8
김성주 <aaa8757k@gmail.com>
2025-08-28 15:20:44 +0900

    chore: (modal)폴더 생성

--- src/app/(profile)/(component)/FollowModalTrigger.tsx
+++ src/app/(profile)/(component)/FollowModalTrigger.tsx
@@ -2,7 +2,7 @@
 
 import { useModalStore } from '@/store/modalStore';
 
-import FollowModalContent from './FollowModalContent';
+import FollowModalContent from './(modal)/FollowModalContent';
 
 interface Props {
   username: string;
