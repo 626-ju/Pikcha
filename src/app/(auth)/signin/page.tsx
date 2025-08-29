40a395f502ddeb971ff9fe0cd0bc41f7690ca262
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-08-29 17:59:58 +0900

    add: 회원가입 이동 버튼 추가

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
 import { signIn as nextAuthSignIn } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 
@@ -52,41 +53,61 @@ const SigninPage = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={cn(
-        'm-auto flex h-full flex-col justify-center gap-[60px]',
-        'w-[335px] pt-[108px]',
-        'md:w-[440px] md:py-[181px]',
-        'xl:w-[640px] xl:py-[90px]',
-      )}
-    >
-      <div className='flex flex-col gap-[30px] md:gap-10'>
-        <Input
-          type='email'
-          label='이메일'
-          placeholder='이메일을 입력해주세요'
-          errorMessage={errors.email?.message}
-          {...register('email')}
-        />
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={cn(
+          'm-auto flex h-full flex-col justify-center gap-[60px]',
+          'w-[335px] pt-[108px] pb-5',
+          'md:w-[440px] md:pt-[181px]',
+          'xl:w-[640px] xl:pt-[90px]',
+        )}
+      >
+        <div className='flex flex-col gap-[30px] md:gap-5'>
+          <Input
+            type='email'
+            label='이메일'
+            placeholder='이메일을 입력해주세요'
+            errorMessage={errors.email?.message}
+            {...register('email')}
+          />
 
-        <Input
-          type='password'
-          label='비밀번호'
-          placeholder='비밀번호를 입력해주세요'
-          errorMessage={errors.password?.message}
-          {...register('password')}
-        />
-
+          <Input
+            type='password'
+            label='비밀번호'
+            placeholder='비밀번호를 입력해주세요'
+            errorMessage={errors.password?.message}
+            {...register('password')}
+          />
+        </div>
+        <Button type='submit' disabled={isSubmitting}>
+          {isSubmitting ? '로그인 중...' : '로그인'}
+        </Button>
         {/* 로그인 실패 시 에러 메시지 표시 */}
-        {errorMessage && <p className='text-sm text-red-500'>{errorMessage}</p>}
+        {errorMessage && <p className='text-center text-sm text-red-500'>{errorMessage}</p>}
+      </form>
+
+      <div className='text-gray-6e6e82 my-5 text-center'>
+        <p>
+          아직 회원이 아니신가요?{' '}
+          <Link href='/signup' className='underline'>
+            지금 바로 가입해 보세요!
+          </Link>
+        </p>
       </div>
 
-      <Button type='submit' disabled={isSubmitting}>
-        {isSubmitting ? '로그인 중...' : '로그인'}
-      </Button>
+      <hr
+        className={cn(
+          'mx-auto',
+          'border-gray-6e6e82 my-5 border-t',
+          'w-[335px]',
+          'md:w-[440px]',
+          'xl:w-[640px]',
+          'opacity-20',
+        )}
+      />
 
-      <div className='flex flex-col gap-5'>
+      <div className='my-5 flex flex-col gap-5'>
         <p className='text-gray-6e6e82 text-center'>SNS로 바로 시작히기</p>
         <div className='flex items-center justify-center gap-5'>
           <button
@@ -112,7 +133,7 @@ const SigninPage = () => {
           </button>
         </div>
       </div>
-    </form>
+    </>
   );
 };
 
