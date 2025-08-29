a771463e8912c390a09408646d65dd5f810233f0
Sophia Na (나소연) <devsophia029@gmail.com>
2025-08-29 10:43:05 +0900

    feat: 랜딩 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getReviewerRanking } from '@/actions/review/reviewer';
+import FloatingButton from '@/components/ui/FloatingButton';
 
 import { searchProducts } from '../actions/productList';
 import MobileCategoryFilter from './_components/MobileCategoryFilter';
@@ -25,23 +26,25 @@ const Home = async ({
   const items = data.list;
 
   return (
-    <div>
-      <div className='flex gap-6'>
+    <div className='pb-32'>
+      <div className='flex items-start gap-6'>
         <Sidebar selected={category} q={q} />
-        <section className='flex-1 overflow-hidden px-[20px]'>
+        <section className='min-w-0 flex-1 overflow-hidden px-[20px]'>
           <ReviewerRank users={users} className='mb-6 pt-[40px] xl:hidden' />
           {q === '' && category === null ? (
-            <div className='flex flex-col gap-[80px]'>
+            <div className='flex flex-col'>
               <MobileCategoryFilter className='mb-6 md:hidden' />
-              <ReviewRank />
-              <RatingRank />
+              <div className='flex flex-col gap-[80px]'>
+                <ReviewRank />
+                <RatingRank />
+              </div>
             </div>
           ) : items.length === 0 ? (
             <NoResult />
           ) : (
             <div>
               <ResultTitle category={category} q={q} />
-              <MobileCategoryFilter className='mb-6 md:hidden' />
+              <MobileCategoryFilter className='mt-3 md:hidden' />
               <SearchResultList
                 initialProducts={items}
                 initialCursor={data.nextCursor}
@@ -52,6 +55,7 @@ const Home = async ({
         </section>
         <ReviewerRank users={users} className='hidden w-0 xl:flex xl:w-[300px] xl:justify-start' />
       </div>
+      <FloatingButton />
     </div>
   );
 };
