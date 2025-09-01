a639300b19a7c2a60c5489eb17370bb84a3384c5
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-02 02:12:46 +0900

    feat: 버튼 내 스타일 추가

--- src/components/common/gnb/buttons/LoginButton.tsx
+++ src/components/common/gnb/buttons/LoginButton.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link';
 
 const LoginButton = () => {
-  return <Link href='/signin'>로그인</Link>;
+  return (
+    <Link
+      href='/signin'
+      className='md:text-mogazoa-14px-400 xl:text-mogazoa-16px-400 text-white-f1f1f5'
+    >
+      로그인
+    </Link>
+  );
 };
 export default LoginButton;
--- src/components/common/gnb/buttons/SignupButton.tsx
+++ src/components/common/gnb/buttons/SignupButton.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
 
 const SignupButton = () => {
-  return <Link href='/signup'>회원가입</Link>;
+  return (
+    <Link
+      href='/signup'
+      className='md:text-mogazoa-14px-400 xl:text-mogazoa-16px-400 text-white-f1f1f5'
+    >
+      회원가입
+    </Link>
+  );
 };
 
 export default SignupButton;
