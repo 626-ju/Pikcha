5c1bf9fd67186409eaf2ac59d9e4b0a4103640fc
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-03 21:29:51 +0900

    chore: import 린트 에러

--- src/app/products/[productId]/components/AddToCompareButton.tsx
+++ src/app/products/[productId]/components/AddToCompareButton.tsx
@@ -3,12 +3,13 @@
 import { toast } from 'sonner';
 
 import CompareConfirmModal from '@/app/compare/components/CompareConfirmModal';
-import CompareOverflowModal from '@/app/compare/components/CompareOverflowModal';
 import Button from '@/components/ui/Buttons';
-import { MAX_COMPARE_ITEMS } from '@/constants/compareNumber';
 import { useCompareStore } from '@/store/compareStore';
 import { useModalStore } from '@/store/modalStore';
 import { Product } from '@/types/product/productType';
+
+import CompareOverflowModal from '@/app/compare/components/CompareOverflowModal';
+import { MAX_COMPARE_ITEMS } from '@/constants/compareNumber';
 
 interface AddToCompareButtonProps {
   product: Product;
