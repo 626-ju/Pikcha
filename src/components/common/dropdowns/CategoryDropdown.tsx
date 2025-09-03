2dbb7071d7340786f99ca8221db0fafde7d01ad8
10000jaeN <akswo256@naver.com>
2025-09-03 13:14:27 +0900

    LIN-59 refactor: 버튼 중첩으로 인한 하이드레이션 에러 수정

--- src/components/common/dropdowns/CategoryDropdown.tsx
+++ src/components/common/dropdowns/CategoryDropdown.tsx
@@ -41,15 +41,15 @@ export const CategoryDropdown = ({ onChange }: { onChange?: (id: number) => void
       {isOpen && (
         <div className='bg-black-252530 no-scrollbar border-black-353542 absolute top-[60px] left-0 z-50 flex max-h-[250px] w-full flex-col gap-2 overflow-y-auto rounded-lg border-1 p-[10px] md:top-[65px] xl:top-[75px]'>
           {categoryArray.map((ca) => (
-            <button
+            <div
               key={ca.id}
-              value={ca.id}
+              role='button'
               tabIndex={0}
               className='hover:bg-black-353542 text-mogazoa-14px-400 text-gray-6e6e82 hover:text-white-f1f1f5 xl:text-mogazoa-16px-400 flex w-full justify-start rounded-[6px] px-5 py-[6px]'
               onClick={() => handleSelectValue(ca)}
             >
               {ca.name}
-            </button>
+            </div>
           ))}
         </div>
       )}
