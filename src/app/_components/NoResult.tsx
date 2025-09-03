86b2c6bb79ccc7d2f54169a99f70c3b56e3c5216
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-03 21:23:46 +0900

    Revert "feat: 공용 버튼으로 변경"
    This reverts commit 55f89faa266dc79940062b85454fef19a8f76164.

--- src/app/_components/NoResult.tsx
+++ src/app/_components/NoResult.tsx
@@ -1,13 +1,17 @@
 import { Clapperboard } from 'lucide-react';
-
-import ReturnToListButton from '@/components/common/ReturnToListButton';
+import Link from 'next/link';
 
 const NoResult = () => {
   return (
     <div className='flex h-screen flex-col items-center justify-start gap-3 pt-[20vh]'>
       <Clapperboard className='text-black-2e2e3a h-[100px] w-[100px]' />
       <h2 className='text-mogazoa-18px-400 text-white-f1f1f5'>검색 결과가 없습니다</h2>
-      <ReturnToListButton />
+      <Link
+        href={'/'}
+        className='bg-main-gradation text-mogazoa-16px-300 inline-block self-center rounded-2xl px-6 py-2.5'
+      >
+        전체 인기 목록 보기
+      </Link>
     </div>
   );
 };
