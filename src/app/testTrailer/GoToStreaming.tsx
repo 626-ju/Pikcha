421f0150a963b35b023469ec041550a12b811665
김성주 <aaa8757k@gmail.com>
2025-09-04 16:40:53 +0900

    refactor: 외부링크 노 오프너 노 레퍼러 추가

--- src/app/testTrailer/GoToStreaming.tsx
+++ src/app/testTrailer/GoToStreaming.tsx
@@ -12,8 +12,14 @@ export const StreamingIcon = ({ providers }: { providers: ProviderInfo[] }) => {
       <h3 className='text-mogazoa-16px-600 text-gray-6e6e82 mb-5'>감상하기</h3>
       <div className='inline-flex items-center'>
         {providers.map((el) => (
-          <a href={el.url} key={el.url} className='mr-10 duration-300 last:mr-0 hover:scale-110'>
-            <Image src={el.logo ?? ''} alt='sd' width={40} height={40} />
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href={el.url}
+            key={el.url}
+            className='mr-10 duration-300 last:mr-0 hover:scale-110'
+          >
+            <Image src={el.logo ?? ''} alt='프로바이더 로고' width={40} height={40} />
           </a>
         ))}
       </div>
