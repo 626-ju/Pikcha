a0ce2f3d82f7e73270996160156f23c8d4c472b3
김성주 <aaa8757k@gmail.com>
2025-09-06 15:09:52 +0900

    refactor:로그아웃 시 홈으로

--- src/app/(profile)/(component)/UpdateTrigger.tsx
+++ src/app/(profile)/(component)/UpdateTrigger.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React from 'react';
-
+import { useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 
 import Button from '@/components/ui/Buttons';
@@ -11,11 +10,17 @@ import ProfileUpdateModal from './(modal)/ProfileUpdateModal';
 
 const UpdateTrigger = () => {
   const openModal = useModalStore((state) => state.openModal);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    signOut({ redirect: false });
+    router.push('/');
+  };
 
   return (
     <div className='w-full'>
       <Button onClick={() => openModal({ component: ProfileUpdateModal })}>프로필 편집</Button>
-      <Button variant='tertiary' onClick={() => signOut()} className='mt-2.5 md:mt-[15px] xl:mt-5'>
+      <Button variant='tertiary' onClick={handleLogout} className='mt-2.5 md:mt-[15px] xl:mt-5'>
         로그아웃
       </Button>
     </div>
