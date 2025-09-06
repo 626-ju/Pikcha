c9da33f03b12f295c309fb9920bb6b7b24fe8a7e
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-09-06 12:35:57 +0900

    add: 미들웨어 설정 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,22 @@ import { auth } from './auth';
 import type { NextRequest } from 'next/server';
 
 const protectedRoutes = ['/mypage', '/compare', '/product', '/user'];
+const authRoutes = ['/signin', '/signup', '/oauth/kakao'];
 
 export default async function middleware(req: NextRequest) {
   const session = await auth();
   const pathname = req.nextUrl.pathname;
   const isProtected = protectedRoutes.some((r) => pathname.startsWith(r));
+  const isAuthRoute = authRoutes.some((r) => pathname.startsWith(r));
 
+  // 로그인전 접근제한(리다이렉트)
   if (isProtected && !session) {
     return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  // 로그인 후 접근제한(리다이렉트)
+  if (isAuthRoute && session) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
   //로그인한 유저가 user/자기 자신 아이디 들어가려고 할 때
