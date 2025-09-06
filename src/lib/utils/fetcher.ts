92eec02dd15a90c48de7ff3ae76383172b2e4bb4
김성주 <aaa8757k@gmail.com>
2025-09-06 19:06:04 +0900

    refactor:에러 던져주는 거 무조건 뒤에온 메세지가 표시되게 평탄화

--- src/lib/utils/fetcher.ts
+++ src/lib/utils/fetcher.ts
@@ -3,11 +3,36 @@ const fetcher = async (url: string, options?: RequestInit) => {
   const data = await res.json();
 
   if (!res.ok) {
-    console.log(JSON.stringify(data, null, 2));
-    throw new Error(data.message);
+    const errMsg = JSON.stringify(data, null, 2);
+    console.log(errMsg);
+
+    const flattenErrMsg = flattenError(data).message;
+    throw new Error(flattenErrMsg);
   }
 
   return data;
 };
 
 export default fetcher;
+
+const flattenError = (data: Record<string, unknown>) => {
+  let topMessage = '';
+
+  if (typeof data.message === 'string') {
+    topMessage = data.message;
+  }
+
+  function recurse(current: object) {
+    for (const val of Object.values(current)) {
+      if (val && typeof val === 'object') {
+        if ('message' in val && typeof val.message === 'string') {
+          topMessage = val.message;
+        }
+        recurse(val);
+      }
+    }
+  }
+
+  recurse(data);
+  return { message: topMessage };
+};
