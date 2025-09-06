ad2a6a21132e3e4ac15ce51c4b7f717212ae6856
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-06 21:15:34 +0900

    feat: 컬럼 추가

--- src/app/_components/ProductList.tsx
+++ src/app/_components/ProductList.tsx
@@ -1,6 +1,6 @@
 const ProductList = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='grid grid-cols-2 gap-4 pt-[30px] md:gap-6 md:pr-10 xl:grid-cols-3'>
+    <div className='grid grid-cols-3 gap-4 pt-[30px] md:gap-6 lg:grid-cols-3 xl:grid-cols-4'>
       {children}
     </div>
   );
