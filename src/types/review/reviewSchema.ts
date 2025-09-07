2ada9afa909d3cc9660238137e320bc0b11444b9
10000jaeN <akswo256@naver.com>
2025-09-07 20:30:03 +0900

    LIN-71 refactor: 리뷰 수정, 삭제 수정, 삭제메세지 모달 파일 이름 더 상세히, 스키마 수정, 모달 여백맞추기, 에러메세지 모달 버튼으로 전달

--- src/types/review/reviewSchema.ts
+++ src/types/review/reviewSchema.ts
@@ -6,19 +6,20 @@ export const postReviewSchema = z.object({
     .max(300, '리뷰내용은 최대 300자까지 가능합니다.')
     .min(10, '최소 10자 이상 적어주세요.')
     .nonempty('리뷰 내용을 입력해주세요.'),
-  rating: z.number({ message: '별점으로 작품을 평가해주세요.' }),
+  rating: z.number().min(1, '별점으로 작품을 평가해주세요.'),
   images: z.array(z.string()).optional(),
 });
 
 export type PostReviewFormValue = z.infer<typeof postReviewSchema>;
 
+// 리팩토링 할 수도 있어서 분리해놓음
 export const patchReviewSchema = z.object({
   content: z
     .string()
     .max(300, '리뷰내용은 최대 300자까지 가능합니다.')
     .min(10, '최소 10자 이상 적어주세요.')
     .nonempty('리뷰 내용을 입력해주세요.'),
-  rating: z.number({ message: '별점으로 작품을 평가해주세요.' }),
+  rating: z.number().min(1, '별점으로 작품을 평가해주세요.'),
   images: z.array(z.string()).optional(),
 });
 
