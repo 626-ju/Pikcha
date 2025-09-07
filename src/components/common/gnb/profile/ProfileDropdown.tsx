4454c4208561626aa85404e42089e0c51be1519f
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-07 16:26:52 +0900

    feat: 프로필 드롭다운 너비, 토글, li 선택 방식 변경

--- src/components/common/gnb/profile/ProfileDropdown.tsx
+++ src/components/common/gnb/profile/ProfileDropdown.tsx
@@ -18,16 +18,19 @@ interface ProfileDropdownProps {
 
 const ProfileDropdown = ({ session }: ProfileDropdownProps) => {
   const [listOpen, setListOpen] = useState(false);
-  const ref = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const compareList = useCompareStore();
   const clearCompareList = compareList.clearCompareList;
 
-  useOnClickOutside(ref, () => {
+  useOnClickOutside(containerRef, () => {
     setListOpen(false);
   });
 
   return (
-    <div className='relative flex h-[70px] items-center px-2 md:h-[80px] xl:h-[100px]'>
+    <div
+      ref={containerRef}
+      className='relative flex h-[70px] items-center px-2 md:h-[80px] xl:h-[100px]'
+    >
       <button
         type='button'
         aria-haspopup='menu'
@@ -39,23 +42,23 @@ const ProfileDropdown = ({ session }: ProfileDropdownProps) => {
       </button>
       {listOpen && (
         <div
-          ref={ref}
           role='menu'
-          className='border-black-353542 bg-black-252530 absolute top-13 left-0 z-10 w-36 rounded-lg border px-2 py-2 xl:right-[120px]'
+          className='border-black-353542 bg-black-252530 absolute top-20 right-0 z-10 w-32 rounded-lg border px-2 py-2'
         >
           <ul>
-            <li className='hover:bg-black-353542 text-white-f1f1f5 rounded-lg px-2 py-1'>
-              <Link href='/mypage' onClick={() => setListOpen(false)}>
+            <li className='hover:bg-black-353542 text-white-f1f1f5 rounded-lg'>
+              <Link href='/mypage' onClick={() => setListOpen(false)} className='block px-2 py-1'>
                 내 프로필
               </Link>
             </li>
-            <li className='hover:bg-black-353542 text-white-f1f1f5 rounded-lg px-2 py-1'>
+            <li className='hover:bg-black-353542 text-white-f1f1f5 rounded-lg'>
               <button
                 onClick={() => {
                   setListOpen(false);
                   clearCompareList();
                   signOut();
                 }}
+                className='w-full px-2 py-1 text-left'
               >
                 로그아웃
               </button>
