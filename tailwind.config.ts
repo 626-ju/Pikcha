4165fd96dba7a2fde7a4e1930b08d51b998c94cd
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-09-08 17:31:52 +0900

    setting: 테마 작업 추가설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-// tailwind.config.ts
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  // 라이트/다크를 버튼으로 토글하려면 class 전략이 필수
-  darkMode: 'class',
+  darkMode: ['class', '.light'],
   content: [
     './src/app/**/*.{ts,tsx,mdx}',
     './src/components/**/*.{ts,tsx,mdx}',
