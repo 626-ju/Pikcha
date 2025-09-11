1691bb36310b30d659262ddc5e214bcfb9e78838
김성주 <aaa8757k@gmail.com>
2025-09-11 17:09:12 +0900

    fix: (profile) 내부  라이트모드 들어간 거 지우기

--- src/app/(profile)/(component)/StatisticsCard.tsx
+++ src/app/(profile)/(component)/StatisticsCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ActivityCard = ({ title, children }: Props) => {
   return (
-    <div className='border-black-353542 bg-black-252530 light:bg-white flex h-[119px] w-full min-w-[92px] flex-col items-center justify-center gap-[15px] rounded-[12px] xl:h-[128px] xl:gap-5'>
+    <div className='border-black-353542 bg-black-252530 flex h-[119px] w-full min-w-[92px] flex-col items-center justify-center gap-[15px] rounded-[12px] xl:h-[128px] xl:gap-5'>
       <h3 className='text-mogazoa-14px-500 xl:text-mogazoa-16px-500 text-gray-9fa6b2 text-center break-keep'>
         {title}
       </h3>
