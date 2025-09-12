ff040f7f589c60afd0b1aff537bd0d023a5c1ce2
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-09-12 23:06:55 +0900

    add: 큰 로고 Link 추가

--- src/components/common/BigLogo.tsx
+++ src/components/common/BigLogo.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 import Logo from '@/assets/icon/PickCha2.svg';
 
 const BicLogo = () => {
   return (
-    <div className='hidden items-center justify-center pt-10 md:flex'>
+    <Link href='/' className='hidden items-center justify-center pt-10 md:flex'>
       <Logo width={200} height={100} />
-    </div>
+    </Link>
   );
 };
 
