f192885a02d02528ba7cc1870af913191068e6d0
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-09-12 22:48:46 +0900

    feat: 큰 로고 컴포넌트 생성

---
+++ src/components/common/BigLogo.tsx
@@ -0,0 +1,11 @@
+import Logo from '@/assets/icon/PickCha2.svg';
+
+const BicLogo = () => {
+  return (
+    <div className='hidden items-center justify-center pt-10 md:flex'>
+      <Logo width={200} height={100} />
+    </div>
+  );
+};
+
+export default BicLogo;
