80d6a09515cc061a6bb5b55fa03509ba31aa06a1
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-12 19:17:54 +0900

    refactor: 우선순위 속성추가

--- src/components/common/gnb/buttons/LogoButton.tsx
+++ src/components/common/gnb/buttons/LogoButton.tsx
@@ -4,7 +4,16 @@ import Link from 'next/link';
 const LogoButton = () => {
   return (
     <Link href='/' className='flex items-center'>
-      <Image src='/icon/PickCha2.svg' alt='Logo' width={110} height={28} />
+      <Image src='/icon/PickCha2.svg' alt='Logo' width={110} height={28} priority />
+      {/* <Image 
+        src='/icon/PickCha2.svg' 
+        alt='Logo' 
+        width={110} 
+        height={28}
+        priority
+        sizes='(max-width: 768px) 90px, 110px'
+        className='h-6 w-auto md:h-7 xl:h-8'
+      /> */}
     </Link>
   );
 };
