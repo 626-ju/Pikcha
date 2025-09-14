da946fea76f3f265df35fffe2860880a83b3cd74
Sophia Na (나소연) <devsophia029@gmail.com>
2025-09-15 01:35:06 +0900

    refactor: 로고 이미지 최적화

--- src/components/common/gnb/buttons/LogoButton.tsx
+++ src/components/common/gnb/buttons/LogoButton.tsx
@@ -4,16 +4,17 @@ import Link from 'next/link';
 const LogoButton = () => {
   return (
     <Link href='/' className='flex items-center'>
-      <Image src='/icon/PickCha2.svg' alt='Logo' width={110} height={28} priority />
-      {/* <Image 
-        src='/icon/PickCha2.svg' 
-        alt='Logo' 
-        width={110} 
+      {/* <Image src='/icon/PickCha2.svg' alt='Logo' width={110} height={28} priority /> */}
+      <Image
+        src='/icon/PickCha2.svg'
+        alt='Logo'
+        width={110}
         height={28}
         priority
         sizes='(max-width: 768px) 90px, 110px'
-        className='h-6 w-auto md:h-7 xl:h-8'
-      /> */}
+        fetchPriority='high'
+        // className='h-6 w-auto md:h-7 xl:h-8'
+      />
     </Link>
   );
 };
